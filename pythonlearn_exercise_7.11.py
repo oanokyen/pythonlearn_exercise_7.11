bd32e912e06fed53517663a6b5c19a688387434a
Oppong-Agyare ANOKYE NKANSAH <72044731+oanokyen@users.noreply.github.com>
2020-11-16 00:34:46 +0000

    Update pythonlearn_exercise_7.11.py

--- pythonlearn_exercise_7.11.py
+++ pythonlearn_exercise_7.11.py
@@ -1,31 +1,5 @@
-# Write a program to read through a file and print the contents
-# of the file (line by line) all in upper case. 
-# Executing the program will look as follows:
-
-# file = input('insert file: ')
-# fhand= open(file)
-# for line in fhand:
-    # ucase = line.upper()
-    # print(ucase)
-
-
-
-
-# Q2   
-# Write a program to prompt for a file name, and then read
-# through the file and look for lines of the form: 
-# X-DSPAM-Confidence: 0.8475
-
-# When you encounter a line that starts with “X-DSPAM-Confidence:”
-# pull apart the line to extract the floating-point number on the line.
-# Count these lines and then compute the total of the spam confidence
-# values from these lines. When you reach the end of the file, print out
-# the average spam confidence.
-
-
-
 file = input('Enter the file name: ')
-fhand= open('file')
+fhand= open(file)
 count= 0
 r_sum=0
 for line in fhand:
